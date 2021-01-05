1ce886b99a3cff05eeaaeb21f02a101bfde246ad
Bateristico <camilo.saa@gmail.com>
2021-01-05 12:08:19 +0100

    13. React Router (and map undefined error)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,14 @@ import GameDetail from '../components/GameDetail';
 //Styling and Animation
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
-
+import { useLocation } from 'react-router-dom';
 
 const Home = () => {
+    //Get Current Location
+    const location = useLocation();
+    const pathId = location.pathname.split("/")[2];
+
+
     //fetch games
     const dispatch = useDispatch();
     useEffect(() => {
@@ -21,7 +26,7 @@ const Home = () => {
 
     return(
         <GameList>
-            <GameDetail />
+           { pathId && <GameDetail /> }
             <h2>Upcoming Games</h2>
             <Games>
                 {upcoming.map(game =>(
