0e4cc7513393bbfea8118afad1a614634c0711f8
Bateristico <camilo.saa@gmail.com>
2021-01-06 09:58:40 +0100

    21. Nav Searching

--- src/api.js
+++ src/api.js
@@ -42,6 +42,9 @@ const newGames = `games?dates=${lastYear},${currenDate}&ordering=-released&page_
  export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
  //Game Screenshots
  export const gameScreenshotURL = (game_id) => `${base_url}games/${game_id}/screenshots`;
+ //Searched Game
+ export const searchGameURL = (game_name) => `${base_url}games?search=${game_name}&page_size=9`;
+
 
 
 
