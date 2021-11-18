a9b9bbb49bc408aebfc4df11faf9b0000dc6fdee
Bateristico <camilo.saa@gmail.com>
2021-11-18 18:46:11 +0100

    fix: add api key needed and refactor code

--- README.md
+++ README.md
@@ -1,26 +1,32 @@
 # React Ignite Webpage
 
-This project connects to a API, returns games info and uses redux for state management 
+This project connects to a API, returns games info and uses redux for state management
 
 # Utilites included:
 
-* [axios](https://www.npmjs.com/package/axios)
-    * Promise based HTTP client for the browser and node.js
+- [axios](https://www.npmjs.com/package/axios)
 
-* [framer-motion](https://www.npmjs.com/package/framer-motion)
-    * An open source and production-ready motion library for React on the web.
+  - Promise based HTTP client for the browser and node.js
 
-* [styled-components](https://www.npmjs.com/package/styled-components)
-    * Visual primitives for the component age. Use the best bits of ES6 and CSS to style your apps without stress.
+- [framer-motion](https://www.npmjs.com/package/framer-motion)
 
-* [redux](https://www.npmjs.com/package/redux)
-    * Redux is a predictable state container for JavaScript apps.    
+  - An open source and production-ready motion library for React on the web.
+
+- [styled-components](https://www.npmjs.com/package/styled-components)
+
+  - Visual primitives for the component age. Use the best bits of ES6 and CSS to style your apps without stress.
+
+- [redux](https://www.npmjs.com/package/redux)
+  - Redux is a predictable state container for JavaScript apps.
 
 ## Setup
 
-
 ```
 npm install
+```
+
+```
+create a .env file in the root folder and add a variable called REACT_APP_API_KEY with the API key value given in https://rawg.io/apidocs
 ```
 
 ## Start
--- src/api.js
+++ src/api.js
@@ -1,23 +1,24 @@
 //Base URL
 const base_url = 'https://api.rawg.io/api/';
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 //Getting the date
 const getCurrentMonth = () => {
-    const month = new Date().getMonth() + 1; //returns an integer from 0 to 11 (????)
-    if (month < 10) {
-        return `0${month}`;
-    } else {
-        return month;
-    }
+  const month = new Date().getMonth() + 1; //returns an integer from 0 to 11 (????)
+  if (month < 10) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
 };
 
 const getCurrentDay = () => {
-    const day = new Date().getDate(); 
-    if (day < 10) {
-        return `0${day}`;
-    } else {
-        return day;
-    }
+  const day = new Date().getDate();
+  if (day < 10) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
 };
 
 //Current day/month/year
@@ -25,28 +26,23 @@ const currentYear = new Date().getFullYear();
 const currentMonth = getCurrentMonth();
 const currentDay = getCurrentDay();
 const currenDate = `${currentYear}-${currentMonth}-${currentDay}`;
-const lastYear =  `${currentYear - 1}-${currentMonth}-${currentDay}`;
-const nextYear =  `${currentYear + 1}-${currentMonth}-${currentDay}`;
-
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 //Popular Games
-const popular_games = `games?dates=${lastYear},${currenDate}&ordering=-rating&page_size=10`;
-const upcomping_games = `games?dates=${currenDate},${nextYear}&ordering=-added&page_size=10`;
-const newGames = `games?dates=${lastYear},${currenDate}&ordering=-released&page_size=10`;
+const popular_games = `games?dates=${lastYear},${currenDate}&ordering=-rating&page_size=10&key=${API_KEY}`;
+const upcomping_games = `games?dates=${currenDate},${nextYear}&ordering=-added&page_size=10&key=${API_KEY}`;
+const newGames = `games?dates=${lastYear},${currenDate}&ordering=-released&page_size=10&key=${API_KEY}`;
 
- export const popularGamesURL = () => `${base_url}${popular_games}`;
- export const upcomingGamesURL = () => `${base_url}${upcomping_games}`;
- export  const newGamesURL = () => `${base_url}${newGames}`;
+export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcomping_games}`;
+export const newGamesURL = () => `${base_url}${newGames}`;
 
- //Game Details
- export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
- //Game Screenshots
- export const gameScreenshotURL = (game_id) => `${base_url}games/${game_id}/screenshots`;
- //Searched Game
- export const searchGameURL = (game_name) => `${base_url}games?search=${game_name}&page_size=9`;
-
-
-
-
-
-
+//Game Details
+export const gameDetailsURL = game_id => `${base_url}games/${game_id}?key=${API_KEY}`;
+//Game Screenshots
+export const gameScreenshotURL = game_id =>
+  `${base_url}games/${game_id}/screenshots?key=${API_KEY}`;
+//Searched Game
+export const searchGameURL = game_name =>
+  `${base_url}games?search=${game_name}&page_size=9&key=${API_KEY}`;
